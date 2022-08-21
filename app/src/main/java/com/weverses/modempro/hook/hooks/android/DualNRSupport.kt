b070d77b42e75c7a57a832c342ff51d2686c99f1
weverse <2364908189@qq.com>
2022-08-21 12:57:20 +0800

    尝试更换方法hook

--- app/src/main/java/com/weverses/modempro/hook/hooks/android/DualNRSupport.kt
+++ app/src/main/java/com/weverses/modempro/hook/hooks/android/DualNRSupport.kt
@@ -1,7 +1,7 @@
 package com.weverses.modempro.hook.hooks.android
 
 import com.github.kyuubiran.ezxhelper.utils.findMethod
-import com.github.kyuubiran.ezxhelper.utils.hookReturnConstant
+import com.github.kyuubiran.ezxhelper.utils.hookAfter
 import com.weverses.modempro.hook.hooks.BaseHook
 import de.robv.android.xposed.XposedBridge
 
@@ -10,7 +10,9 @@ object DualNRSupport : BaseHook() {
         try {
             findMethod("miui.telephony.TelephonyManagerEx") {
                 name == "isDualNrSupported"
-            }.hookReturnConstant(true)
+            }.hookAfter {
+                it.result = true
+            }
             XposedBridge.log("ModemX55Pro: Hook isDualNrSupported success!")
         } catch (e: Throwable) {
             XposedBridge.log("ModemX55Pro: Hook isDualNrSupported failed!")
